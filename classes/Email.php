02e7e7fdb67ca13c6f6d242b6aeb468a8f08b30b
facundomv01 <facundoaguilar084@hotmail.com>
2023-09-05 21:15:02 -0300

    first commit

--- classes/Email.php
+++ classes/Email.php
@@ -21,11 +21,11 @@ class Email {
 
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = '';
+        $mail->Host = $_ENV['EMAIL_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = '';
-        $mail->Password = '';
+        $mail->Port =  $_ENV['EMAIL_PORT'];
+        $mail->Username =  $_ENV['EMAIL_USER'];
+        $mail->Password =  $_ENV['EMAIL_PASS'];
 
         $mail->setFrom('cuentas@uptask.com');
         $mail->addAddress('cuentas@uptask.com', 'uptask.com');
@@ -52,11 +52,11 @@ class Email {
 
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = 'smtp.mailtrap.io';
+        $mail->Host = $_ENV['EMAIL_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = '';
-        $mail->Password = '';
+        $mail->Port =  $_ENV['EMAIL_PORT'];
+        $mail->Username =  $_ENV['EMAIL_USER'];
+        $mail->Password =  $_ENV['EMAIL_PASS'];
 
         $mail->setFrom('cuentas@uptask.com');
         $mail->addAddress('cuentas@uptask.com', 'uptask.com');
@@ -67,7 +67,7 @@ class Email {
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Parece que has olvidado tu password, sigue el siguiente enlace para recuperarlo</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/reestablecer?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='". $_ENV['APP_URL']   ."/reestablecer?token=" . $this->token . "'>Reestablecer Password</a></p>";
         $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
         $contenido .= '</html>';
 
